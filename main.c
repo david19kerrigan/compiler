78141234d2ddf774b11a56ab4c894e1e63031c17
David <david19kerrigan@gmail.com>
2024-10-23 23:01:49 -0400

    auto commit

--- main.c
+++ main.c
@@ -2,21 +2,35 @@
 #include <string.h>
 #include <stdlib.h>
 
+int DEFAULT_ARR_SIZE = 32;
+
 int main(){
     FILE *fptr = fopen("input.c", "r");
-    int *stack[] = malloc(32);
+    int stack_size = DEFAULT_ARR_SIZE;
+    int *stack = malloc(stack_size * sizeof(int));
+    int stack_ind = 0;
     int sum = 0;
     while(feof(fptr) == 0){
         char in = fgetc(fptr);
         if(strchr("0123456789", in)){
+            stack[stack_ind++] = in - '0';
         } else if(strchr("abcdefghijklmnopqrstuvwxyz", in)){
         } else if(in == '+'){
-            sum += (in - '0');
+            //sum += (in - '0');
         } else if(in == '-'){
         } else if(in == '*'){
         } else if(in == '/'){
         }
+        if(stack_ind == stack_size-1){
+            stack = realloc(stack, stack_size * 2 * sizeof(int));
+            stack_size *= 2;
+        }
     }
+    printf("\n");
+    for(int i = 0; i < stack_ind; ++i){
+        printf("%d ", stack[i]);
+    }
+    free(stack);
     printf("%d\n", sum);
     pclose(fptr);
     return 0;

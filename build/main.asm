568a1ab9699b9f6d0734c354f58334eb6e25daa3
David <david19kerrigan@gmail.com>
2024-11-29 21:10:02 -0500

    update readme

--- build/main.asm
+++ build/main.asm
@@ -13,13 +13,11 @@ pop rbx
 add rax, rbx 
 push rax 
 
-; set |a|
 pop rax 
 mov [rbp-32], rax 
 
 push 3 
 push 2 
-; recall |a|
 mov rax, [rbp-32] 
 push rax 
 
@@ -39,13 +37,11 @@ pop rbx
 imul rax, rbx 
 push rax 
 
-; set |b|
 pop rax 
 mov [rbp-64], rax 
 
 push 8 
 push 5 
-; recall |a|
 mov rax, [rbp-32] 
 push rax 
 
@@ -54,7 +50,6 @@ pop rax
 sub rax, rbx 
 push rax 
 
-; recall |b|
 mov rax, [rbp-64] 
 push rax 
 
@@ -80,21 +75,17 @@ pop rbx
 imul rax, rbx 
 push rax 
 
-; set |c|
 pop rax 
 mov [rbp-96], rax 
 
-; recall |a|
 mov rax, [rbp-32] 
 push rax 
 
 push 2 
-; recall |b|
 mov rax, [rbp-64] 
 push rax 
 
 push 3 
-; recall |c|
 mov rax, [rbp-96] 
 push rax 
 
